5c6ea7acdba0b576027ecf14ca369b52f51bf101
Andrew Wichmann <andyp.wichmann@gmail.com>
2024-02-04 21:58:01 +0000

    working HTTP serviver

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	chat "github.com/Andrew-Wichmann/AI-Resume/cmd/api/openai"
@@ -9,19 +10,21 @@ import (
 
 func resumeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
-	chatResponse, err := chat.GetResponse([]openai.ChatCompletionMessage{
-		{
-			Role:    openai.ChatMessageRoleUser,
-			Content: "You are an assistant created by Andrew Wichmann. Your primary function is to assist him in advertising his skills to potential employers. Following this chat, you will be speaking with potential employers. You are instructed only to speak about Andrew in the context of his accomplishment listed in this chat. Please be polite. When asked about what you can do, please respond that you can only offer biographical information, contact information, and speak about Andrew's career accomplishments. The following is Andrew's basic biographical information; Andrew is a 29 year old software engineer with 6 years of experience. He has a Bachelor's degree in Computer Science from the University of Southern Illinois University in Carbondale. He is located in New York City and he is open to a hybrid work environment. His target salary is $150,000.",
-		},
-		{
-			Role:    openai.ChatMessageRoleUser,
-			Content: "Hello, I'm the CEO of Big Software Inc. I'm interested in hiring Andrew. Can you tell me more about him?",
-		},
-	})
+
+	var messages []openai.ChatCompletionMessage
+
+	err := json.NewDecoder(r.Body).Decode(&messages)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
+		return
+	}
+
+	chatResponse, err := chat.GetResponse(messages)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(chatResponse))
@@ -29,5 +32,9 @@ func resumeHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/resume", resumeHandler)
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	http.ListenAndServe(":8080", nil)
 }
--- cmd/api/openai/chat.go
+++ cmd/api/openai/chat.go
@@ -1,3 +1,4 @@
+// Package chat provides an abstraction for communicating with OpenAI's API.
 package chat
 
 import (
@@ -8,11 +9,21 @@ import (
 	"github.com/sashabaranov/go-openai"
 )
 
+var initialPrompt = []openai.ChatCompletionMessage{
+	{
+		Role:    openai.ChatMessageRoleUser,
+		Content: "You are an assistant created by Andrew Wichmann. Your primary function is to assist him in advertising his skills to potential employers. Following this chat, you will be speaking with potential employers. You are instructed only to speak about Andrew in the context of his accomplishment listed in this chat. Please be polite. When asked about what you can do, please respond that you can only offer biographical information, contact information, and speak about Andrew's career accomplishments. The following is Andrew's basic biographical information; Andrew is a 29 year old software engineer with 6 years of experience. He has a Bachelor's degree in Computer Science from the University of Southern Illinois University in Carbondale. He is located in New York City and he is open to a hybrid work environment. His target salary is $150,000.",
+	},
+}
+
 func GetResponse(chats []openai.ChatCompletionMessage) (string, error) {
 	token, set := os.LookupEnv("OPENAI_API_TOKEN")
 	if !set {
 		panic("Token not provided")
 	}
+
+	chats = append(initialPrompt, chats...)
+
 	client := openai.NewClient(token)
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
