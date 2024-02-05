26f05090c531e758bea5750c95535a5913ccb5ba
Andrew Wichmann <awichmann@mintel.com>
2024-02-05 04:40:05 +0000

    basic web UI

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"encoding/json"
 	"net/http"
 	"os"
@@ -37,6 +38,25 @@ func resumeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func lambdaHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if request.Path == "/resume" {
+
+	} else if request.Path == "/health" {
+	} else if request.Path == "/" {
+		webPage, err := webPage.ReadFile("web/static/index.html")
+		if err != nil {
+			return events.APIGatewayProxyResponse{
+				StatusCode: http.StatusInternalServerError,
+				Body:       err.Error(),
+			}, nil
+		}
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusOK,
+			Body:       string(webPage),
+			Headers: map[string]string{
+				"Content-Type": "text/html",
+			},
+		}, nil
+	}
 	var messages []openai.ChatCompletionMessage
 	err := json.NewDecoder(strings.NewReader(request.Body)).Decode(&messages)
 	if err != nil {
@@ -59,6 +79,9 @@ func lambdaHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProx
 	}, nil
 }
 
+//go:embed web/static/index.html
+var webPage embed.FS
+
 func main() {
 	println("Starting server")
 	mode, ok := os.LookupEnv("SERVER_MODE")
@@ -67,6 +90,7 @@ func main() {
 	}
 
 	if mode == "HTTP_SERVER" {
+		http.Handle("/", http.FileServer(http.FS(webPage)))
 		http.HandleFunc("/resume", resumeHandler)
 		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
